167294dab0d246893b01866c9788bac7a1fcdb7a
Paolo Patierno <ppatierno@live.com>
2021-01-13 21:24:37 +0100

    Add skeleton code (#1)
    * Added operator skeleton code
    
    * Removed getName to fix NPE
    
    * Updated to quarkus operator extension 1.6.2
    
    * Added minimum logic to run controller

---
+++ src/main/java/org/bf2/operator/AgentOperator.java
@@ -0,0 +1,33 @@
+package org.bf2.operator;
+
+import javax.inject.Inject;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.javaoperatorsdk.operator.Operator;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@QuarkusMain
+public class AgentOperator implements QuarkusApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(AgentOperator.class);
+
+    @Inject
+    private KubernetesClient client;
+
+    @Inject
+    private Operator operator;
+
+    @Inject
+    private ManagedKafkaController managedKafkaController;
+
+    @Override
+    public int run(String... args) throws Exception {
+        log.info("Managed Kafka agent operator");
+        Quarkus.waitForExit();
+        return 0;
+    }
+}
---
+++ src/main/java/org/bf2/operator/api/v1alpha1/KafkaInstance.java
@@ -0,0 +1,14 @@
+package org.bf2.operator.api.v1alpha1;
+
+public class KafkaInstance {
+
+    private String version;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
