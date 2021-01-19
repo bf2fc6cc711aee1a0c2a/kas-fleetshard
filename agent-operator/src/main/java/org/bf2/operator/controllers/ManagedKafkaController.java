956ecd0693979c30ca27517279302d3b653f6226
Paolo Patierno <ppatierno@live.com>
2021-01-19 12:37:31 +0100

    Added event source handling for Kafka resource related changes Added draft of ManagedKafka status update from the Kafka resource status

--- agent-operator/src/main/java/org/bf2/operator/controllers/ManagedKafkaController.java
+++ agent-operator/src/main/java/org/bf2/operator/controllers/ManagedKafkaController.java
@@ -5,18 +5,35 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
-import io.javaoperatorsdk.operator.api.*;
+import io.javaoperatorsdk.operator.api.Controller;
+import io.javaoperatorsdk.operator.api.Context;
+import io.javaoperatorsdk.operator.api.DeleteControl;
+import io.javaoperatorsdk.operator.api.ResourceController;
+import io.javaoperatorsdk.operator.api.UpdateControl;
 import io.javaoperatorsdk.operator.processing.event.EventSourceManager;
+import io.javaoperatorsdk.operator.processing.event.internal.CustomResourceEvent;
 import io.strimzi.api.kafka.Crds;
 import io.strimzi.api.kafka.KafkaList;
 import io.strimzi.api.kafka.model.DoneableKafka;
 import io.strimzi.api.kafka.model.Kafka;
+import io.strimzi.api.kafka.model.status.Condition;
+import org.bf2.operator.KafkaEvent;
+import org.bf2.operator.KafkaEventSource;
 import org.bf2.operator.KafkaFactory;
 import org.bf2.operator.resources.v1alpha1.ManagedKafka;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaCondition;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaConditionBuilder;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaStatus;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaStatusBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
 
 @Controller(crdName = "managedkafkas.managedkafka.bf2.org", name = "ManagedKafkaController")
 public class ManagedKafkaController implements ResourceController<ManagedKafka> {
@@ -27,6 +44,8 @@ public class ManagedKafkaController implements ResourceController<ManagedKafka> 
     private KubernetesClient client;
 
     private MixedOperation<Kafka, KafkaList, DoneableKafka, Resource<Kafka, DoneableKafka>> kafkaClient;
+
+    private KafkaEventSource kafkaEventSource;
 
     @Override
     public DeleteControl deleteResource(ManagedKafka managedKafka, Context<ManagedKafka> context) {
@@ -42,15 +61,66 @@ public class ManagedKafkaController implements ResourceController<ManagedKafka> 
 
     @Override
     public UpdateControl<ManagedKafka> createOrUpdateResource(ManagedKafka managedKafka, Context<ManagedKafka> context) {
-        log.info("Creating Kafka instance version {}", managedKafka.getSpec().getKafkaInstance().getVersion());
 
-        Kafka kafka = KafkaFactory.getKafka(managedKafka);
-        log.info("Creating Kafka = {}", kafka);
-        try {
-            kafkaClient.create(kafka);
-        } catch (Exception e) {
-            log.error("Error creating the Kafka instance", e);
+        Optional<CustomResourceEvent> latestManagedKafkaEvent =
+                context.getEvents().getLatestOfType(CustomResourceEvent.class);
+
+        if (latestManagedKafkaEvent.isPresent()) {
+            // Kafka resource doesn't exist, has to be created
+            if (kafkaClient.withName(managedKafka.getMetadata().getName()).get() == null) {
+                Kafka kafka = KafkaFactory.getKafka(managedKafka);
+                log.info("Creating Kafka instance {}/{}", kafka.getMetadata().getNamespace(), kafka.getMetadata().getName());
+                try {
+                    kafkaClient.create(kafka);
+                } catch (Exception e) {
+                    log.error("Error creating the Kafka instance", e);
+                    return UpdateControl.noUpdate();
+                }
+
+                // TODO: factoring out a better mapping of statuses
+                ManagedKafkaStatus status = managedKafka.getStatus();
+                if (status == null) {
+                    ManagedKafkaCondition condition = new ManagedKafkaConditionBuilder()
+                            .withReason("KafkaResourceCreated")
+                            .withMessage("Kafka resource created")
+                            .withType("Ready")
+                            .withStatus("True")
+                            .withLastTransitionTime(iso8601Now())
+                            .build();
+                    status = new ManagedKafkaStatusBuilder()
+                            .withConditions(condition)
+                            .build();
+                    managedKafka.setStatus(status);
+                }
+                return UpdateControl.updateCustomResourceAndStatus(managedKafka);
+            // Kafka resource already exists, has to be updated
+            } else {
+                log.info("Updating Kafka instance {}", managedKafka.getSpec().getKafkaInstance().getVersion());
+                // TODO: updating Kafka instance
+                return UpdateControl.noUpdate();
+            }
+
         }
+
+        Optional<KafkaEvent> latestKafkaEvent =
+                context.getEvents().getLatestOfType(KafkaEvent.class);
+        if (latestKafkaEvent.isPresent()) {
+            Kafka kafka = latestKafkaEvent.get().getKafka();
+            log.info("Kafka resource {}/{} is changed", kafka.getMetadata().getNamespace(), kafka.getMetadata().getName());
+            if (kafka.getStatus() != null) {
+                log.info("Kafka conditions = {}", kafka.getStatus().getConditions());
+                Condition kafkaCondition = kafka.getStatus().getConditions().get(0);
+                // TODO: doing a better mapping, right now it's just reflecting the Kafka resource status
+                ManagedKafkaCondition managedKafkaCondition = managedKafka.getStatus().getConditions().get(0);
+                managedKafkaCondition.setReason(kafkaCondition.getReason());
+                managedKafkaCondition.setMessage(kafkaCondition.getMessage());
+                managedKafkaCondition.setType(kafkaCondition.getType());
+                managedKafkaCondition.setStatus(kafkaCondition.getStatus());
+                managedKafkaCondition.setLastTransitionTime(iso8601Now());
+            }
+            return UpdateControl.updateCustomResourceAndStatus(managedKafka);
+        }
+
         return UpdateControl.noUpdate();
     }
 
@@ -61,5 +131,16 @@ public class ManagedKafkaController implements ResourceController<ManagedKafka> 
         CustomResourceDefinition kafkaCrd = Crds.kafka();
         CustomResourceDefinitionContext kafkaCrdContext = CustomResourceDefinitionContext.fromCrd(kafkaCrd);
         kafkaClient = client.customResources(kafkaCrdContext, Kafka.class, KafkaList.class, DoneableKafka.class);
+
+        kafkaEventSource = KafkaEventSource.createAndRegisterWatch(kafkaClient);
+        eventSourceManager.registerEventSource("kafka-event-source", kafkaEventSource);
+    }
+
+    /**
+     * Returns the current timestamp in ISO 8601 format, for example "2019-07-23T09:08:12.356Z".
+     * @return the current timestamp in ISO 8601 format, for example "2019-07-23T09:08:12.356Z".
+     */
+    public static String iso8601Now() {
+        return ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
     }
 }
---
+++ agent-operator/src/main/java/org/bf2/operator/resources/v1alpha1/ManagedKafkaCondition.java
@@ -0,0 +1,54 @@
+package org.bf2.operator.resources.v1alpha1;
+
+import io.sundr.builder.annotations.Buildable;
+
+@Buildable
+public class ManagedKafkaCondition {
+
+    private String type;
+    private String reason;
+    private String message;
+    private String status;
+    private String lastTransitionTime;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLastTransitionTime() {
+        return lastTransitionTime;
+    }
+
+    public void setLastTransitionTime(String lastTransitionTime) {
+        this.lastTransitionTime = lastTransitionTime;
+    }
+
+}
