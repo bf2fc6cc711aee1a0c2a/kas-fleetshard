253b0656c13960033f5de648387a0a101f2e12cf
shawkins <shawkins@redhat.com>
2021-01-22 10:04:18 -0500

    adding a sharedindexinformer

--- agent-sync/README.md
+++ agent-sync/README.md
@@ -2,10 +2,11 @@
 
 - the agent-operator will need to declare the usage of this sidecar.  I added the kubernetes-annotations jar for that, but realized there nowhere to use that given the operator framework.  So that's probably not the right approach - maybe just configuration is all that is needed.
 
-Presuming that the architecture will follow the path of a kube connection from the agent sync to the control plane we have:
-
 - This module will likely need to depend upon the agent module as we'll be shared the ManagedKafka cr.
 
-- For the kube connection to control plane it's not clear yet how that will happen.  In the proposal docs it's mentioned that the credentials would be rotated.  Presumably there will be a Secret that will need to be checked on connection failure or something.
+Presuming that the architecture of an http connection from agent-sync to the control plane:
 
-- The full set of information that the control plane needs should be captured somewhere.  The agent sync may be able to just copy the full status section of the ManagedKafka cr, so there may not need to be deep logic here.
+
+- The full set of information that the control plane needs should be captured somewhere.  The agent sync may be able to just copy the full status section of the ManagedKafka cr, so there may not need to be deep logic here.  The Agent resource should also provide status containing the used/available managed kafka units.
+
+- From https://kubernetes.io/docs/reference/using-api/api-concepts/ there is a Reflector concept that can be used for watching / Storing an object.  There is an even higher level SharedInformer.
--- agent-sync/src/main/java/org/bf2/sync/AgentSync.java
+++ agent-sync/src/main/java/org/bf2/sync/AgentSync.java
@@ -2,10 +2,16 @@ package org.bf2.sync;
 
 import javax.inject.Inject;
 
+import org.bf2.operator.resources.v1alpha1.ManagedKafka;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.dekorate.crd.config.Scope;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
+import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
+import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 
@@ -16,12 +22,47 @@ public class AgentSync implements QuarkusApplication {
     @Inject
     private KubernetesClient localClient;
 
-    //we can't inject this as the quarkus logic supports a singleton client
-    private KubernetesClient remoteClient;
-
+    //http connection to poll for ManagedKafkas and post updates
+    
     @Override
     public int run(String... args) throws Exception {
         log.info("Managed Kafka agent sync");
+        
+        //CustomResourceDefinition kafkaCrd = Crds.kafka();
+        //CustomResourceDefinitionContext kafkaCrdContext = CustomResourceDefinitionContext.fromCrd(kafkaCrd);
+        
+        //monitor the ManagedKafka resources
+        
+        CustomResourceDefinitionContext managedKafkaCrdContext = new CustomResourceDefinitionContext.Builder()
+        		.withScope(Scope.Namespaced.name())
+        		.withGroup("managedkafka.bf2.org")
+        		.withVersion("v1alpha1")
+        		.withKind("ManagedKafka")
+        		.withPlural("managedkafkas")
+        		.withName("managedkafkas.managedkafka.bf2.org").build();
+        		
+        SharedIndexInformer<ManagedKafka> managedKafkaInformer = localClient.informers()
+        		.sharedIndexInformerForCustomResource(managedKafkaCrdContext, ManagedKafka.class, ManagedKafkaList.class, 1000);
+        
+        managedKafkaInformer.addEventHandler(new ResourceEventHandler<ManagedKafka>() {
+			
+			@Override
+			public void onUpdate(ManagedKafka oldObj, ManagedKafka newObj) {
+				System.out.print("update " + oldObj);
+			}
+			
+			@Override
+			public void onDelete(ManagedKafka obj, boolean deletedFinalStateUnknown) {
+				System.out.print("delete " + obj);
+			}
+			
+			@Override
+			public void onAdd(ManagedKafka obj) {
+				System.out.print("add " + obj);
+			}
+		});
+        
+        //monitor the agent to supply "kafka units"
         
         Quarkus.waitForExit();
         return 0;
