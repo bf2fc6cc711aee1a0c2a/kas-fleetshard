e88b7d3ed6c39a0cfce535ebe5acc3e1cf15d815
Paolo Patierno <ppatierno@live.com>
2021-01-25 18:04:01 +0100

    Moved from watcher to informers for Kafka resource

--- agent-operator/src/main/java/org/bf2/operator/KafkaEvent.java
+++ agent-operator/src/main/java/org/bf2/operator/KafkaEvent.java
@@ -6,20 +6,14 @@ import io.strimzi.api.kafka.model.Kafka;
 
 public class KafkaEvent extends AbstractEvent {
 
-    private Watcher.Action action;
     private Kafka kafka;
 
-    public KafkaEvent(Watcher.Action action, Kafka kafka, KafkaEventSource kafkaEventSource) {
+    public KafkaEvent(Kafka kafka, KafkaEventSource kafkaEventSource) {
         super(kafka.getMetadata().getOwnerReferences().get(0).getUid(), kafkaEventSource);
-        this.action = action;
         this.kafka = kafka;
     }
 
     public Kafka getKafka() {
         return kafka;
-    }
-
-    public Watcher.Action getAction() {
-        return action;
     }
 }
--- agent-operator/src/main/java/org/bf2/operator/KafkaEventSource.java
+++ agent-operator/src/main/java/org/bf2/operator/KafkaEventSource.java
@@ -4,55 +4,39 @@ import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
+import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
 import io.javaoperatorsdk.operator.processing.event.AbstractEventSource;
 import io.strimzi.api.kafka.KafkaList;
 import io.strimzi.api.kafka.model.Kafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KafkaEventSource extends AbstractEventSource implements Watcher<Kafka> {
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class KafkaEventSource extends AbstractEventSource implements ResourceEventHandler<Kafka> {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaEventSource.class);
 
-    private MixedOperation<Kafka, KafkaList, Resource<Kafka>> kafkaClient;
-
-    private KafkaEventSource(MixedOperation<Kafka, KafkaList, Resource<Kafka>> kafkaClient) {
-        this.kafkaClient = kafkaClient;
-    }
-
-    public static KafkaEventSource createAndRegisterWatch(MixedOperation<Kafka, KafkaList, Resource<Kafka>> kafkaClient) {
-        KafkaEventSource kafkaEventSource = new KafkaEventSource(kafkaClient);
-        kafkaEventSource.registerWatch();
-        return kafkaEventSource;
-    }
-
-    private void registerWatch() {
-        kafkaClient
-                .inAnyNamespace()
-                .watch(this);
+    @Override
+    public void onAdd(Kafka kafka) {
+        log.info("Add event receiving for Kafka {}/{}", kafka.getMetadata().getNamespace(), kafka.getMetadata().getName());
+        handleEvent(kafka);
     }
 
     @Override
-    public void eventReceived(Action action, Kafka kafka) {
-        log.info("Kafka event received: action {} kafka {}/{}", action, kafka.getMetadata().getNamespace(), kafka.getMetadata().getName());
-        if (action == Action.ERROR) {
-            log.warn("Skipping Kafka event: action {} kafka {}/{}", action, kafka.getMetadata().getNamespace(), kafka.getMetadata().getName());
-            return;
-        }
-        eventHandler.handleEvent(new KafkaEvent(action, kafka, this));
+    public void onUpdate(Kafka oldKafka, Kafka newKafka) {
+        log.info("Update event receiving for Kafka {}/{}", oldKafka.getMetadata().getNamespace(), oldKafka.getMetadata().getName());
+        handleEvent(newKafka);
     }
 
     @Override
-    public void onClose(WatcherException e) {
-        if (e == null) {
-            return;
-        }
-        if (e.isHttpGone()) {
-            log.warn("Received error for watch, will try to reconnect.", e);
-            registerWatch();
-        } else {
-            log.error("Unexpected error happened with watch. Will exit.", e);
-            System.exit(1);
-        }
+    public void onDelete(Kafka kafka, boolean deletedFinalStateUnknown) {
+        log.info("Delete event receiving for Kafka {}/{}", kafka.getMetadata().getNamespace(), kafka.getMetadata().getName());
+        handleEvent(kafka);
+    }
+
+    private void handleEvent(Kafka kafka) {
+        eventHandler.handleEvent(new KafkaEvent(kafka, this));
     }
 }
