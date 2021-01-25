0dbaa6da8c053a6ac2dac6e1a540d04be5a8dcce
Paolo Patierno <ppatierno@live.com>
2021-01-25 21:58:16 +0100

    Bumped new dekorate version Used new Crd annotation

--- agent-api/src/main/java/org/bf2/operator/resources/v1alpha1/ManagedKafka.java
+++ agent-api/src/main/java/org/bf2/operator/resources/v1alpha1/ManagedKafka.java
@@ -1,5 +1,6 @@
 package org.bf2.operator.resources.v1alpha1;
 
+import io.dekorate.crd.annotation.Crd;
 import io.dekorate.crd.annotation.Status;
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
@@ -8,7 +9,7 @@ import io.fabric8.kubernetes.model.annotation.Version;
 
 @Group("managedkafka.bf2.org")
 @Version("v1alpha1")
-@io.dekorate.crd.annotation.CustomResource(group = "managedkafka.bf2.org", version = "v1alpha1")
+@Crd(group = "managedkafka.bf2.org", version = "v1alpha1")
 public class ManagedKafka extends CustomResource<ManagedKafkaSpec, ManagedKafkaStatus> implements Namespaced {
 
     private ManagedKafkaSpec spec;
