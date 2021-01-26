1ab9d6c9e030b2ae8c7a6eb1f255e6c29c80c150
shawkins <shawkins@redhat.com>
2021-01-26 08:49:07 -0500

    adding in a rough in for the control plane calls

--- agent-sync/src/main/java/org/bf2/sync/AgentSync.java
+++ agent-sync/src/main/java/org/bf2/sync/AgentSync.java
@@ -4,45 +4,49 @@ import javax.inject.Inject;
 
 import org.bf2.operator.resources.v1alpha1.ManagedKafka;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaList;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.dekorate.crd.config.Scope;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
 import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.scheduler.Scheduled;
 
 public class AgentSync implements QuarkusApplication {
 
     private static final Logger log = LoggerFactory.getLogger(AgentSync.class);
 
+    //TODO: where should this be coming from
+    @ConfigProperty(name = "cluster.id") 
+    String id;
+    
     @Inject
-    KubernetesClient localClient;
-
-    //http connection to poll for ManagedKafkas and post updates
+    KubernetesClient client;
+    
+    @Inject
+    @RestClient
+    ControlPlaneRestClient controlPlane;
     
     @Override
     public int run(String... args) throws Exception {
         log.info("Managed Kafka agent sync");
         
-        //CustomResourceDefinition kafkaCrd = Crds.kafka();
-        //CustomResourceDefinitionContext kafkaCrdContext = CustomResourceDefinitionContext.fromCrd(kafkaCrd);
-        
         //monitor the ManagedKafka resources
         
-        CustomResourceDefinitionContext managedKafkaCrdContext = new CustomResourceDefinitionContext.Builder()
-        		.withScope(Scope.Namespaced.name())
-        		.withGroup("managedkafka.bf2.org")
-        		.withVersion("v1alpha1")
-        		.withKind("ManagedKafka")
-        		.withPlural("managedkafkas")
-        		.withName("managedkafkas.managedkafka.bf2.org").build();
+		CustomResourceDefinition managedKafkaCrd = client.apiextensions().v1().customResourceDefinitions()
+				.withName("managedkafkas.managedkafka.bf2.org").get();
+
+		CustomResourceDefinitionContext managedKafkaCrdContext = CustomResourceDefinitionContext
+				.fromCrd(managedKafkaCrd);
         		
         int resyncPeriodInMillis = 60000;
-		SharedIndexInformer<ManagedKafka> managedKafkaInformer = localClient.informers()
+		SharedIndexInformer<ManagedKafka> managedKafkaInformer = client.informers()
         		.sharedIndexInformerForCustomResource(managedKafkaCrdContext, ManagedKafka.class, ManagedKafkaList.class, resyncPeriodInMillis);
         
         managedKafkaInformer.addEventHandler(new ResourceEventHandler<ManagedKafka>() {
@@ -50,27 +54,43 @@ public class AgentSync implements QuarkusApplication {
 			@Override
 			public void onUpdate(ManagedKafka oldObj, ManagedKafka newObj) {
 				//an update will also be generated for each resyncPeriodInMillis
-				System.out.println("update " + oldObj);
-				System.out.println(newObj.getStatus());
+				//TODO: filter unnecessary updates
+				controlPlane.updateKafkaClusterStatus(newObj.getStatus(), id, getClusterId(newObj));
 			}
 			
 			@Override
 			public void onDelete(ManagedKafka obj, boolean deletedFinalStateUnknown) {
-				System.out.println("delete " + obj);
+				//TODO: ensure the delete status is set
+				controlPlane.updateKafkaClusterStatus(obj.getStatus(), id, getClusterId(obj));
 			}
 			
 			@Override
 			public void onAdd(ManagedKafka obj) {
-				//on a restart we'll hit add again for each resource
-				System.out.println("add " + obj);
+				//TODO: on a restart we'll hit add again for each resource - that could be filtered
+				controlPlane.updateKafkaClusterStatus(obj.getStatus(), id, getClusterId(obj));
 			}
+
 		});
         
         managedKafkaInformer.run();
         
         //monitor the agent to supply "kafka units"
+        //controlPlane.updateStatus(obj, id);
         
         Quarkus.waitForExit();
         return 0;
     }
+    
+    @Scheduled(every="{poll.interval}")     
+    void pollKafkaClusters() {
+    	//process the response
+    	//TODO: the remote call could be none blocking
+    	//controlPlane.getKafkaClusters(id); 
+    }
+    
+    //TODO: create a method on the ManagedKafka object?
+    private String getClusterId(ManagedKafka obj) {
+		return obj.getMetadata().getAnnotations().get("kas.redhat.com/kafka-id");
+	}
+
 }
---
+++ agent-sync/src/main/java/org/bf2/sync/ControlPlaneRestClient.java
@@ -0,0 +1,38 @@
+package org.bf2.sync;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.bf2.operator.resources.v1alpha1.ManagedKafka;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgentStatus;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaStatus;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/api/managed-services-api/v1/")
+@RegisterRestClient
+public interface ControlPlaneRestClient {
+
+	@PUT
+	@Path("/agent-clusters/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void updateStatus(ManagedKafkaAgentStatus status, @PathParam("id") String id);
+	
+	@GET
+	@Path("/agent-clusters/{id}/kafkas")
+	@Produces(MediaType.APPLICATION_JSON)
+	List<ManagedKafka> getKafkaClusters(@PathParam("id") String id);
+	
+	@PATCH
+	@Path("/agent-clusters/{id}/kafkas/{cluster-id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	void updateKafkaClusterStatus(ManagedKafkaStatus status, @PathParam("id") String id, @PathParam("cluster-id") String clusterId);
+	
+}
