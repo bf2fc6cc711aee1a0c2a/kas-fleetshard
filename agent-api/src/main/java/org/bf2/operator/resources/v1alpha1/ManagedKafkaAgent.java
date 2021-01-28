945d6e9d04b47c719d5aea11351b1dbdb7dfd5a0
Ramesh Reddy <rareddy@jboss.org>
2021-01-28 14:17:23 -0600

    MGDSTRM-1297: Scoffolding for ManagedKafkaAgent

---
+++ agent-api/src/main/java/org/bf2/operator/resources/v1alpha1/ManagedKafkaAgent.java
@@ -0,0 +1,36 @@
+package org.bf2.operator.resources.v1alpha1;
+
+import io.dekorate.crd.annotation.Crd;
+import io.dekorate.crd.annotation.Status;
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("managedkafka.bf2.org")
+@Version("v1alpha1")
+@Crd(group = "managedkafka.bf2.org", version = "v1alpha1")
+public class ManagedKafkaAgent extends CustomResource<ManagedKafkaAgentSpec, ManagedKafkaAgentStatus>
+		implements Namespaced {
+	private static final long serialVersionUID = 1L;
+		
+	private ManagedKafkaAgentSpec spec;
+	@Status
+	private ManagedKafkaAgentStatus status;
+	
+	public ManagedKafkaAgentSpec getSpec() {
+		return spec;
+	}
+	
+	public void setSpec(ManagedKafkaAgentSpec spec) {
+		this.spec = spec;
+	}
+	
+	public ManagedKafkaAgentStatus getStatus() {
+		return status;
+	}
+	
+	public void setStatus(ManagedKafkaAgentStatus status) {
+		this.status = status;
+	}
+}
---
+++ agent-api/src/main/java/org/bf2/operator/resources/v1alpha1/ManagedKafkaAgentList.java
@@ -0,0 +1,6 @@
+package org.bf2.operator.resources.v1alpha1;
+
+import io.fabric8.kubernetes.client.CustomResourceList;
+
+public class ManagedKafkaAgentList extends CustomResourceList<ManagedKafkaAgent> {
+}
