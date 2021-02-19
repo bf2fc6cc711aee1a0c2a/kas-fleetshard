6169ca4ffaa954e54c71894d79591439cb784dbd
Ramesh Reddy <rareddy@jboss.org>
2021-02-19 12:08:08 -0600

    MGDSTRM-1791: checking for empty version string and setting Managed Kafaka agent CR accordingly

--- kas-fleetshard-sync/src/main/java/org/bf2/sync/informer/ManagedKafkaAgentCRHandler.java
+++ kas-fleetshard-sync/src/main/java/org/bf2/sync/informer/ManagedKafkaAgentCRHandler.java
@@ -6,28 +6,26 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
-import io.quarkus.runtime.StartupEvent;
-
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgent;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgentBuilder;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgentList;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgentSpecBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
-import org.jose4j.base64url.Base64;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 /**
  * TODO: This is throw away code until the Control Plane API for ManagedkafkaAgent CR is defined.
  */
 @ApplicationScoped
 public class ManagedKafkaAgentCRHandler implements ResourceEventHandler<Secret> {
+    private static final String STRIMZI_ALLOWED_VERSIONS = "strimzi.allowed_versions";
     public static final String SECRET_NAME = "addon-kas-fleetshard-operator-parameters";
     private static final String RESOURCE_NAME = "managed-agent";
 
@@ -45,16 +43,6 @@ public class ManagedKafkaAgentCRHandler implements ResourceEventHandler<Secret> 
     @PostConstruct
     void onStart() {
         this.agentClient = kubernetesClient.customResources(ManagedKafkaAgent.class, ManagedKafkaAgentList.class);
-    }
-
-    void onStartUp(@Observes StartupEvent ev) {
-        Secret secret = this.kubernetesClient.secrets()
-                .inNamespace(this.kubernetesClient.getNamespace())
-                .withName(SECRET_NAME).get();
-
-        if (secret != null) {
-            createOrUpdateManagedKafkaAgentCR(secret);
-        }
     }
 
     @Override
@@ -83,27 +71,36 @@ public class ManagedKafkaAgentCRHandler implements ResourceEventHandler<Secret> 
 
     private void createOrUpdateManagedKafkaAgentCR(Secret secret) {
         String namespace = this.kubernetesClient.getNamespace();
-        String[] allowedVersions = new String(Base64.decode(secret.getData().get("strimzi.allowed_versions")))
-                .trim().split("\\s*,\\s*");
+        String versions = secret.getData().get(STRIMZI_ALLOWED_VERSIONS);
 
         ManagedKafkaAgent resource = this.agentClient
                 .inNamespace(this.kubernetesClient.getNamespace())
                 .withName(RESOURCE_NAME).get();
 
-        if (resource == null) {
-            resource = new ManagedKafkaAgentBuilder()
-                    .withSpec(new ManagedKafkaAgentSpecBuilder()
-                            .withClusterId(this.clusterId)
-                            .withAllowedStrimziVersions(allowedVersions)
-                            .build())
-                    .withMetadata(new ObjectMetaBuilder().withName(RESOURCE_NAME)
-                            .withNamespace(namespace)
-                            .build())
-                    .build();
-            this.agentClient.inNamespace(namespace).createOrReplace(resource);
-        } else if (!Arrays.equals(resource.getSpec().getAllowedStrimziVersions(), allowedVersions)) {
-            resource.getSpec().setAllowedStrimziVersions(allowedVersions);
-            this.agentClient.inNamespace(namespace).createOrReplace(resource);
+        if (versions != null && versions.trim().length() > 0) {
+            String[] allowedVersions = base64Decode(versions).trim().split("\\s*,\\s*");
+            if (resource == null) {
+                resource = new ManagedKafkaAgentBuilder()
+                        .withSpec(new ManagedKafkaAgentSpecBuilder()
+                                .withClusterId(this.clusterId)
+                                .withAllowedStrimziVersions(allowedVersions)
+                                .build())
+                        .withMetadata(new ObjectMetaBuilder().withName(RESOURCE_NAME)
+                                .withNamespace(namespace)
+                                .build())
+                        .build();
+                this.agentClient.inNamespace(namespace).createOrReplace(resource);
+            } else if (!Arrays.equals(resource.getSpec().getAllowedStrimziVersions(), allowedVersions)) {
+                resource.getSpec().setAllowedStrimziVersions(allowedVersions);
+                this.agentClient.inNamespace(namespace).createOrReplace(resource);
+            }
+        } else if (resource != null) {
+            // this is not really allowed, allowed versions from CRD is required property
+            log.error("The property " + STRIMZI_ALLOWED_VERSIONS + " is not set in add-on secret " + SECRET_NAME);
         }
+    }
+
+    private static String base64Decode(String str) {
+        return new String(Base64.getDecoder().decode(str));
     }
 }
