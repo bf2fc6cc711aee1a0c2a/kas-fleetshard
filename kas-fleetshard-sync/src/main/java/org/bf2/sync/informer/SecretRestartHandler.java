f552b34e9fc5e1062a364a8d87debcd540ac9533
shawkins <shawkins@redhat.com>
2021-02-23 08:34:19 -0500

    adding handling for properties from secret
    adding a container restart to deal with a changing secret
    adding a set of simple properties for use from the addon secret
    using quarkus-kubernetes-config which will eventually give us the exact
    initial secret used and the ability to make the secret optional

---
+++ kas-fleetshard-sync/src/main/java/org/bf2/sync/informer/SecretRestartHandler.java
@@ -0,0 +1,45 @@
+package org.bf2.sync.informer;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import io.fabric8.kubernetes.api.model.Secret;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.scheduler.Scheduled;
+
+@ApplicationScoped
+public class SecretRestartHandler {
+    @Inject
+    Logger log;
+
+    @ConfigProperty(name="secret.name")
+    String secretName;
+
+    @Inject
+    KubernetesClient client;
+
+    private volatile String resourceVersion;
+    private volatile String uid;
+
+    @Scheduled(every = "60s")
+    public void checkSecret() {
+        Secret secret = client.secrets().inNamespace(client.getNamespace()).withName(secretName).get();
+        if (secret == null) {
+            return;
+        }
+        if (resourceVersion == null) {
+            // capture the initial version being used
+            // will be replaced by https://github.com/quarkusio/quarkus/issues/15247
+            resourceVersion = secret.getMetadata().getResourceVersion();
+            uid = secret.getMetadata().getUid();
+        } else if (!resourceVersion.equals(secret.getMetadata().getResourceVersion())
+                    || !uid.equals(secret.getMetadata().getUid())) {
+            log.info(secretName + " changed, requires a restart to pickup new configuration");
+            Quarkus.asyncExit();
+        }
+    }
+}
