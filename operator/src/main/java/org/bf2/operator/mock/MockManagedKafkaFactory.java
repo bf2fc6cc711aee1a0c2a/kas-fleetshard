4919fcda92752f756a97a1194faf3548349e1ebf
Steven Hawkins <shawkins@redhat.com>
2021-03-24 12:18:11 -0400

    MGDSTRM-2421 consolidating ManagedKafka creation logic (#199)

--- operator/src/main/java/org/bf2/operator/mock/MockManagedKafkaFactory.java
+++ operator/src/main/java/org/bf2/operator/mock/MockManagedKafkaFactory.java
@@ -1,12 +1,10 @@
 package org.bf2.operator.mock;
 
-import io.fabric8.kubernetes.api.model.Quantity;
 import io.quarkus.arc.properties.IfBuildProperty;
 import io.quarkus.scheduler.Scheduled;
+
 import org.bf2.common.ManagedKafkaResourceClient;
 import org.bf2.operator.resources.v1alpha1.ManagedKafka;
-import org.bf2.operator.resources.v1alpha1.ManagedKafkaSpecBuilder;
-import org.bf2.operator.resources.v1alpha1.VersionsBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
@@ -14,15 +12,12 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.Map;
 import java.util.Random;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @ApplicationScoped
 @IfBuildProperty(name = "kafka", stringValue = "test")
 public class MockManagedKafkaFactory {
-
-    private static final String CERT = "cert";
 
     @Inject
     Logger log;
@@ -39,45 +34,6 @@ public class MockManagedKafkaFactory {
     // Unique Id for the clusters
     private AtomicInteger clusterIdGenerator = new AtomicInteger(1);
 
-    private ManagedKafka createManagedKafka(int id) {
-        ManagedKafka mk = new ManagedKafka();
-        mk.setSpec(new ManagedKafkaSpecBuilder()
-                .withVersions(new VersionsBuilder().withKafka("2.6.0").withStrimzi("0.21.1").build())
-                .withNewCapacity()
-                    .withIngressEgressThroughputPerSec(Quantity.parse("2Mi"))
-                    .withTotalMaxConnections(100)
-                    .withMaxDataRetentionPeriod("P14D")
-                    .withMaxDataRetentionSize(Quantity.parse("50Gi"))
-                    .withMaxPartitions(100)
-                .endCapacity()
-                .withNewOauth()
-                    .withClientId("clientId")
-                    .withClientSecret("secret")
-                    .withUserNameClaim("claim")
-                    .withJwksEndpointURI("http://jwks")
-                    .withTokenEndpointURI("https://token")
-                    .withValidIssuerEndpointURI("http://issuer")
-                    .withUserNameClaim("claim")
-                    .withTlsTrustedCertificate(CERT)
-                .endOauth()
-                .withNewEndpoint()
-                    .withBootstrapServerHost("xyz.com")
-                    .withNewTls()
-                        .withCert(CERT)
-                        .withKey(CERT)
-                    .endTls()
-                .endEndpoint()
-                .build());
-        mk.setId(clusterName(id));
-        mk.setPlacementId(UUID.randomUUID().toString());
-        mk.getMetadata().setName("kluster-" + clusterName(id));
-        return mk;
-    }
-
-    private String clusterName(int i) {
-        return "kafka-" + i;
-    }
-
     @Scheduled(every = "{mock.factory.interval}", delayed = "10s")
     void loop() {
 
@@ -88,7 +44,7 @@ public class MockManagedKafkaFactory {
         if (this.kafkas.size() == 0) {
             int max = Math.abs(random.nextInt(maxKafkas));
             for (int i = 0; i < max; i++) {
-                ManagedKafka k = createManagedKafka(this.clusterIdGenerator.getAndIncrement());
+                ManagedKafka k = ManagedKafka.getDummyInstance(this.clusterIdGenerator.getAndIncrement());
                 log.infof("Mock ManagedKafka Factory::marking %s for addition", k.getId());
                 this.kafkas.put(k.getId(), k);
                 mkClient.create(k);
@@ -111,7 +67,7 @@ public class MockManagedKafkaFactory {
 
         // selectively add
         if (this.kafkas.size() < maxKafkas && random.nextBoolean()) {
-            ManagedKafka k = createManagedKafka(this.clusterIdGenerator.getAndIncrement());
+            ManagedKafka k = ManagedKafka.getDummyInstance(this.clusterIdGenerator.getAndIncrement());
             log.infof("Mock ManagedKafka Factory:: creating a new cluster %s ", k.getId());
             this.kafkas.put(k.getId(), k);
             mkClient.create(k);
