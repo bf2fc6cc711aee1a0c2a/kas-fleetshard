04b597f7d058046f5df1b94811b7f7ca2a3fdfda
shawkins <shawkins@redhat.com>
2021-03-26 08:32:01 -0400

    removing clustercapacitydelta and clearing up various warnings

--- sync/src/test/java/org/bf2/sync/PollerTest.java
+++ sync/src/test/java/org/bf2/sync/PollerTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
