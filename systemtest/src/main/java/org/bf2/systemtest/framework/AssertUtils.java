c6726377d8c88723da492395640978f079e5eaad
David Kornel <kornys@outlook.com>
2021-04-13 14:52:41 +0200

    Create AssertUtils to remove redundant code (#275)
    * Create AssertUtils to remove redundant code

---
+++ systemtest/src/main/java/org/bf2/systemtest/framework/AssertUtils.java
@@ -0,0 +1,54 @@
+package org.bf2.systemtest.framework;
+
+import io.strimzi.api.kafka.KafkaList;
+import io.strimzi.api.kafka.model.Kafka;
+import org.bf2.operator.resources.v1alpha1.ManagedKafka;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgentStatus;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaCondition;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaStatus;
+import org.bf2.systemtest.framework.resource.ManagedKafkaResourceType;
+import org.bf2.test.k8s.KubeClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AssertUtils {
+
+    public static void assertManagedKafka(ManagedKafka mk) {
+        KubeClient kube = KubeClient.getInstance();
+        var kafkacli = kube.client().customResources(Kafka.class, KafkaList.class);
+
+        assertNotNull(ManagedKafkaResourceType.getOperation().inNamespace(mk.getMetadata().getNamespace()).withName(mk.getMetadata().getName()).get());
+        assertNotNull(kafkacli.inNamespace(mk.getMetadata().getNamespace()).withName(mk.getMetadata().getName()).get());
+        assertTrue(kube.client().pods().inNamespace(mk.getMetadata().getNamespace()).list().getItems().size() > 0);
+        assertEquals("Running", ManagedKafkaResourceType.getCanaryPod(mk).getStatus().getPhase());
+        assertEquals("Running", ManagedKafkaResourceType.getAdminApiPod(mk).getStatus().getPhase());
+        assertEquals(3, ManagedKafkaResourceType.getKafkaPods(mk).size());
+        if (!kube.isGenericKubernetes()) {
+            assertEquals(1, ManagedKafkaResourceType.getKafkaExporterPods(mk).size());
+        }
+        assertEquals(3, ManagedKafkaResourceType.getZookeeperPods(mk).size());
+    }
+
+    public static void assertManagedKafkaStatus(ManagedKafka mk, ManagedKafkaStatus mkStatus) {
+        assertEquals(mk.getStatus().getAdminServerURI(), mkStatus.getAdminServerURI());
+        assertEquals(mk.getStatus().getCapacity().getTotalMaxConnections(), mkStatus.getCapacity().getTotalMaxConnections());
+        assertEquals(mk.getStatus().getCapacity().getIngressEgressThroughputPerSec(), mkStatus.getCapacity().getIngressEgressThroughputPerSec());
+        assertEquals(mk.getStatus().getCapacity().getMaxDataRetentionPeriod(), mkStatus.getCapacity().getMaxDataRetentionPeriod());
+        assertEquals(mk.getStatus().getCapacity().getMaxPartitions(), mkStatus.getCapacity().getMaxPartitions());
+        assertEquals(mk.getStatus().getCapacity().getMaxDataRetentionSize(), mkStatus.getCapacity().getMaxDataRetentionSize());
+        mkStatus.getConditions()
+                .forEach(condition -> assertTrue(ManagedKafkaResourceType.hasConditionStatus(mk,
+                        ManagedKafkaCondition.Type.valueOf(condition.getType()),
+                        ManagedKafkaCondition.Status.valueOf(condition.getStatus()))));
+    }
+
+    public static void assertManagedKafkaAgentStatus(ManagedKafkaAgentStatus agentStatus) {
+        assertEquals(1, agentStatus.getConditions().size());
+        assertNotNull(agentStatus.getTotalCapacity());
+        assertNotNull(agentStatus.getRemainingCapacity());
+        assertNotNull(agentStatus.getResizeInfo());
+        assertNotNull(agentStatus.getRequiredNodeSizes());
+    }
+}
--- systemtest/src/test/java/org/bf2/systemtest/integration/OperatorSyncST.java
+++ systemtest/src/test/java/org/bf2/systemtest/integration/OperatorSyncST.java
@@ -3,8 +3,6 @@ package org.bf2.systemtest.integration;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.utils.Serialization;
-import io.strimzi.api.kafka.KafkaList;
-import io.strimzi.api.kafka.model.Kafka;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bf2.operator.resources.v1alpha1.ManagedKafka;
@@ -12,6 +10,7 @@ import org.bf2.operator.resources.v1alpha1.ManagedKafkaAgentStatus;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaCondition;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaStatus;
 import org.bf2.systemtest.api.SyncApiClient;
+import org.bf2.systemtest.framework.AssertUtils;
 import org.bf2.systemtest.framework.TestTags;
 import org.bf2.systemtest.framework.resource.ManagedKafkaResourceType;
 import org.bf2.systemtest.operator.FleetShardOperatorManager;
@@ -28,7 +27,6 @@ import java.net.http.HttpResponse;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OperatorSyncST extends AbstractST {
@@ -55,7 +53,6 @@ public class OperatorSyncST extends AbstractST {
     void testCreateManagedKafkaUsingSync(ExtensionContext extensionContext) throws Exception {
         String mkAppName = "mk-test-deploy-api";
         ManagedKafka mk = ManagedKafkaResourceType.getDefault(mkAppName, mkAppName);
-        var kafkacli = kube.client().customResources(Kafka.class, KafkaList.class);
 
         //Create mk using api
         resourceManager.addResource(extensionContext, new NamespaceBuilder().withNewMetadata().withName(mkAppName).endMetadata().build());
@@ -88,34 +85,16 @@ public class OperatorSyncST extends AbstractST {
                 .readValue(SyncApiClient.getManagedKafkaStatus(mk.getId(), syncEndpoint).body(), ManagedKafkaStatus.class);
         ManagedKafka managedKafka = ManagedKafkaResourceType.getOperation().inNamespace(mkAppName).withName(mkAppName).get();
 
-        assertEquals(managedKafka.getStatus().getAdminServerURI(), apiStatus.getAdminServerURI());
-        assertEquals(managedKafka.getStatus().getCapacity().getTotalMaxConnections(), apiStatus.getCapacity().getTotalMaxConnections());
-        assertEquals(managedKafka.getStatus().getCapacity().getIngressEgressThroughputPerSec(), apiStatus.getCapacity().getIngressEgressThroughputPerSec());
-        assertEquals(managedKafka.getStatus().getCapacity().getMaxDataRetentionPeriod(), apiStatus.getCapacity().getMaxDataRetentionPeriod());
-        assertEquals(managedKafka.getStatus().getCapacity().getMaxPartitions(), apiStatus.getCapacity().getMaxPartitions());
-        assertEquals(managedKafka.getStatus().getCapacity().getMaxDataRetentionSize(), apiStatus.getCapacity().getMaxDataRetentionSize());
-        apiStatus.getConditions()
-                .forEach(condition -> assertTrue(ManagedKafkaResourceType.hasConditionStatus(managedKafka,
-                        ManagedKafkaCondition.Type.valueOf(condition.getType()),
-                        ManagedKafkaCondition.Status.valueOf(condition.getStatus()))));
+        AssertUtils.assertManagedKafkaStatus(managedKafka, apiStatus);
 
         //Get agent status
         ManagedKafkaAgentStatus agentStatus = Serialization.jsonMapper()
                 .readValue(SyncApiClient.getManagedKafkaAgentStatus(syncEndpoint).body(), ManagedKafkaAgentStatus.class);
-        assertEquals(1, agentStatus.getConditions().size());
-        assertNotNull(agentStatus.getTotalCapacity());
-        assertNotNull(agentStatus.getRemainingCapacity());
-        assertNotNull(agentStatus.getResizeInfo());
-        assertNotNull(agentStatus.getRequiredNodeSizes());
+
+        AssertUtils.assertManagedKafkaAgentStatus(agentStatus);
 
         //Check if managed kafka deployed all components
-        assertNotNull(ManagedKafkaResourceType.getOperation().inNamespace(mkAppName).withName(mkAppName).get());
-        assertNotNull(kafkacli.inNamespace(mkAppName).withName(mkAppName).get());
-        assertTrue(kube.client().pods().inNamespace(mkAppName).list().getItems().size() > 0);
-        assertEquals("Running", ManagedKafkaResourceType.getCanaryPod(mk).getStatus().getPhase());
-        assertEquals("Running", ManagedKafkaResourceType.getAdminApiPod(mk).getStatus().getPhase());
-        assertEquals(3, ManagedKafkaResourceType.getKafkaPods(mk).size());
-        assertEquals(3, ManagedKafkaResourceType.getZookeeperPods(mk).size());
+        AssertUtils.assertManagedKafka(mk);
 
         //delete mk using api
         res = SyncApiClient.deleteManagedKafka(mk.getId(), syncEndpoint);
