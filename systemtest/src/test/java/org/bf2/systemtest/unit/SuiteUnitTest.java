a22c654a1e303e3829360f5f0e93f4d8a3edf2d5
David Kornel <kornys@outlook.com>
2021-05-18 13:22:11 +0200

    Remove mock kubernetes server workaround

--- systemtest/src/test/java/org/bf2/systemtest/unit/SuiteUnitTest.java
+++ systemtest/src/test/java/org/bf2/systemtest/unit/SuiteUnitTest.java
@@ -7,15 +7,17 @@ import io.fabric8.kubernetes.api.model.PodListBuilder;
 import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.kubernetes.client.KubernetesServerTestResource;
+import io.quarkus.test.kubernetes.client.KubernetesTestServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bf2.systemtest.framework.ParallelTest;
+import org.bf2.systemtest.framework.SequentialTest;
 import org.bf2.systemtest.integration.AbstractST;
 import org.bf2.test.executor.Exec;
 import org.bf2.test.executor.ExecBuilder;
 import org.bf2.test.executor.ExecResult;
 import org.bf2.test.k8s.KubeClusterException;
-import org.bf2.test.mock.QuarkusKubeMockServer;
-import org.bf2.test.mock.QuarkusKubernetesMockServer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -30,13 +32,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Tests for verifying that test suite is not broken and basic functionality is working properly
  */
 @QuarkusTest
-@QuarkusTestResource(QuarkusKubeMockServer.class)
+@QuarkusTestResource(KubernetesServerTestResource.class)
 public class SuiteUnitTest extends AbstractST {
     private static final Logger LOGGER = LogManager.getLogger(SuiteUnitTest.class);
     private static final String TEST_NS = "default";
 
-    @QuarkusKubernetesMockServer
-    KubernetesServer server;
+    @KubernetesTestServer
+    KubernetesServer mockServer;
 
     @BeforeAll
     void setupMockServer() {
@@ -46,23 +48,23 @@ public class SuiteUnitTest extends AbstractST {
                 new PodBuilder().withNewMetadata().withName("pod2").withNamespace(TEST_NS).endMetadata()
                         .build()).build();
         for (Pod p : expectedPodList.getItems()) {
-            server.getClient().pods().inNamespace(TEST_NS).create(p);
+            mockServer.getClient().pods().inNamespace(TEST_NS).create(p);
         }
     }
 
-    @Test
+    @ParallelTest
     void testKubeConnection() {
         List<Pod> pods = kube.client().pods().inNamespace(TEST_NS).list().getItems();
         pods.forEach(pod -> LOGGER.info("Found pod with name {}", pod.getMetadata().getName()));
         assertTrue(pods.size() > 0);
     }
 
-    @Test
+    @ParallelTest
     void testKubeRequestFail() {
         assertNull(kube.client().serviceAccounts().withName("non-exists").get());
     }
 
-    @Test
+    @ParallelTest
     void testExecutor() {
         ExecResult result = Exec.builder()
                 .withCommand("ls", System.getProperty("user.dir"))
@@ -73,7 +75,7 @@ public class SuiteUnitTest extends AbstractST {
         assertTrue(result.exitStatus());
     }
 
-    @Test
+    @SequentialTest
     void testExecutorError() {
         ExecBuilder command = Exec.builder()
                 .withCommand("ppppeeeepppaaa", "jenda")
