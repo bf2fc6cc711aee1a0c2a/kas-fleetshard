7ea4a38613073d80f63b4065079417351c0de504
Ramesh Reddy <rareddy@jboss.org>
2021-07-12 12:00:21 -0500

    using a configmap as configuration for fleetshard operator

--- perf/src/main/java/org/bf2/performance/AdopterProfile.java
+++ perf/src/main/java/org/bf2/performance/AdopterProfile.java
@@ -5,16 +5,16 @@ import org.bf2.operator.operands.KafkaInstanceConfiguration;
 public class AdopterProfile {
 
     public static final KafkaInstanceConfiguration VALUE_PROD = buildProfile(
-            "4Gi", "2Gi", "1000m",
-            "8Gi", "3Gi", "3000m");
+            "4Gi", "1G", "1000m",
+            "8Gi", "3G", "3000m");
 
     public static final KafkaInstanceConfiguration SMALL_VALUE_PROD = buildProfile(
-            "1Gi", "500Mi", "500m",
-            "1Gi", "500Mi", "1000m");
+            "1Gi", "500M", "500m",
+            "1Gi", "500M", "1000m");
 
     public static final KafkaInstanceConfiguration TYPE_KICKER = buildProfile(
-            "2Gi", "1Gi", "500m",
-            "2Gi", "1Gi", "500m");
+            "2Gi", "1G", "500m",
+            "2Gi", "1G", "500m");
 
     public static KafkaInstanceConfiguration buildProfile(String zookeeperContainerMemory, String zookeeperJavaMemory,
             String zookeeperCpu, String kafkaContainerMemory, String kafkaJavaMemory, String kafkaCpu) {
@@ -22,6 +22,7 @@ public class AdopterProfile {
         config.getKafka().setContainerMemory(kafkaContainerMemory);
         config.getKafka().setContainerCpu(kafkaCpu);
         config.getKafka().setJvmXms(kafkaJavaMemory);
+        config.getKafka().setEnableQuota(false);
         config.getZookeeper().setContainerCpu(zookeeperCpu);
         config.getZookeeper().setContainerMemory(zookeeperContainerMemory);
         config.getZookeeper().setJvmXms(zookeeperJavaMemory);
