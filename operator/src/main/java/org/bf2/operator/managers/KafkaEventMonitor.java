62096072b71cdb6acdba5d0f5a71677c9d793232
Ramesh Reddy <rareddy@jboss.org>
2021-11-02 14:58:49 -0500

    changing the event selection based namespaces that are determined by label placed on them by fleetshard operator

--- operator/src/main/java/org/bf2/operator/managers/KafkaEventMonitor.java
+++ operator/src/main/java/org/bf2/operator/managers/KafkaEventMonitor.java
@@ -1,15 +1,23 @@
 package org.bf2.operator.managers;
 
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceList;
 import io.fabric8.kubernetes.api.model.events.v1.Event;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.Watcher;
 import io.fabric8.kubernetes.client.WatcherException;
+import io.fabric8.kubernetes.client.dsl.FilterWatchListDeletable;
 import io.quarkus.runtime.Startup;
+import org.bf2.common.OperandUtils;
+import org.bf2.common.ResourceInformer;
+import org.bf2.common.ResourceInformerFactory;
 import org.jboss.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+
+import java.util.Map;
 
 @Startup
 @ApplicationScoped
@@ -21,8 +29,16 @@ public class KafkaEventMonitor {
     @Inject
     KubernetesClient kubernetesClient;
 
+    @Inject
+    ResourceInformerFactory resourceInformerFactory;
+
+    ResourceInformer<Namespace> namespaceInformer;
+
     @PostConstruct
     protected void onStart() {
+        FilterWatchListDeletable<Namespace, NamespaceList> namespaceFilter = kubernetesClient.namespaces()
+                .withLabels(Map.of(OperandUtils.MANAGED_BY_LABEL, OperandUtils.FLEETSHARD_OPERATOR_NAME));
+        namespaceInformer = resourceInformerFactory.create(Namespace.class, namespaceFilter, null);
         startWatch();
     }
 
@@ -32,6 +48,8 @@ public class KafkaEventMonitor {
     }
 
     class KafkaEventWatcher implements Watcher<Event> {
+        private static final String NORMAL = "Normal";
+
         @Override
         public void eventReceived(Action action, Event obj) {
             if (action != Action.DELETED) {
@@ -51,11 +69,12 @@ public class KafkaEventMonitor {
         }
 
         private boolean isKafkaEvent(Event obj) {
-            return obj.getMetadata().getNamespace().startsWith("kafka-") || obj.getMetadata().getNamespace().startsWith("mk-");
+            return namespaceInformer.getList().stream()
+                    .anyMatch(ns -> ns.getMetadata().getName().equals(obj.getMetadata().getNamespace()));
         }
 
         private void logEvent(Event obj) {
-            if (isKafkaEvent(obj) && !obj.getType().equals("Normal")) {
+            if (!NORMAL.equals(obj.getType()) && isKafkaEvent(obj)) {
                 log.warnf("event received: %s/%s/%s/%s", obj.getMetadata().getNamespace(), obj.getType(), obj.getReason(), obj.getNote());
             }
         }
