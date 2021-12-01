bf2fc6cc711aee1a0c2a/kas-fleetshard

commit d7d30c4d598091df76a4c10de067114699ce55c0

updates based upon recent perf testing (#550)
* updates based upon recent perf testing

- override global acls
- account for nodes not having taints initially
- don't add the broker toleration to the ingress replicas
- added a script to update the env with minimal inputs
- aligned the purge script with gitlab

* updating for s3 bucket errors

operator/src/main/java/org/bf2/operator/managers/IngressControllerManager.java
@@ -25,7 +25,6 @@ import org.bf2.common.OperandUtils;
 import org.bf2.common.ResourceInformer;
 import org.bf2.common.ResourceInformerFactory;
 import org.bf2.operator.operands.AbstractKafkaCluster;
-import org.bf2.operator.operands.KafkaCluster;
 import org.bf2.operator.resources.v1alpha1.ManagedKafka;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaRoute;
 import org.jboss.logging.Logger;
@@ -261,19 +260,6 @@ public class IngressControllerManager {
                      .endLoadBalancer()
                 .endEndpointPublishingStrategy()
             .endSpec();
-
-        if (topologyValue != null && !topologyValue.isEmpty()) {
-            builder
-                .editSpec()
-                    .withNewNodePlacement()
-                        .editOrNewNodeSelector()
-                            .addToMatchLabels(TOPOLOGY_KEY, topologyValue)
-                            .addToMatchLabels(WORKER_NODE_LABEL, "")
-                        .endNodeSelector()
-                        .addToTolerations(KafkaCluster.buildKafkaBrokerToleration())
-                    .endNodePlacement()
-                .endSpec();
-        }
 
         return builder.build();
     }

perf/scripts/purge_velero_backup.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/env bash
-# Usage: purge_velero_backup.sh <AWS credentials CSV>
+# Usage: purge_velero_backup.sh
 
-set -e
-
-SED=sed
-GREP=grep
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SED=gsed
+# Install AWS CLI
+if ! [ -x "$(command -v aws)" ]; then
+  echo "Installing the AWS CLI..."
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip -qq awscliv2.zip
+  sudo ./aws/install
+  sudo rm -rf ./aws
+  echo "AWS CLI successfully installed"
 fi
-AWS_CSV_PATH=$1
 
-cred=$($SED '2q;d' "${AWS_CSV_PATH}")
-AWS_ACCESS_KEY="$(cut -d',' -f1 <<<"${cred//[$'\t\r\n']/}")"
-AWS_SECRET_ACCESS_KEY="$(cut -d',' -f2 <<<"${cred//[$'\t\r\n']/}")"
-
-trap 'rm -rf "$TMPFILE"' EXIT
-TMPFILE=$(mktemp -d) || exit 1
-chmod og-r ${TMPFILE}
-
-AWS_CONFIG_FILE=$TMPFILE
-aws configure --profile default set aws_access_key_id ${AWS_ACCESS_KEY}
-aws configure --profile default set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+# Configure the AWS CLI
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID" --profile default
+aws configure set secret_access_key "$AWS_SECRET_ACCESS_KEY" --profile default
+aws configure set region "$AWS_REGION" --profile default
 
 for i in $(aws s3api list-buckets | jq -r '.Buckets[].Name' | grep velero-backups )
 do
 aws s3 rb --force s3://$i
 done
-

perf/scripts/update_env.sh
@@ -1,0 +1,17 @@
+#!/usr/bin/env bash
+# Usage: . update_env.sh <AWS credentials CSV> <region>
+
+set -e
+
+SED=sed
+GREP=grep
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    SED=gsed
+fi
+export AWS_SEC_CREDENTIALS_FILE=$1
+
+cred=$($SED '2q;d' "${AWS_SEC_CREDENTIALS_FILE}")
+export AWS_ACCESS_KEY_ID="$(cut -d',' -f1 <<<"${cred//[$'\t\r\n']/}")"
+export AWS_SECRET_ACCESS_KEY="$(cut -d',' -f2 <<<"${cred//[$'\t\r\n']/}")"
+export AWS_REGION=$2
+export AWS_ID="$(grep -Eo '[0-9]*' <<< $(basename -- ${AWS_SEC_CREDENTIALS_FILE}))"

perf/src/main/java/org/bf2/performance/AdopterProfile.java
@@ -37,6 +37,12 @@ public class AdopterProfile {
         config.getZookeeper().setContainerMemory(zookeeperContainerMemory);
         config.getZookeeper().setJvmXms(zookeeperJavaMemory);
         config.getKafka().getAcl().setAllowedListeners("TLS-9093,SRE-9096"); // by-pass canary acl
+        config.getKafka().getAcl().setGlobal("default=true;permission=allow;topic=*;operations=all \n"
+                + "default=true;permission=allow;group=*;operations=all \n"
+                + "default=true;permission=allow;transactional_id=*;operations=all \n"
+                + "default=true;permission=allow;cluster=*;operations=describe \n"
+                + "permission=allow;cluster=*;operations=idempotent_write \n"
+                + "permission=deny;cluster=*;operations-except=alter,describe,idempotent_write");
         return config;
     }
 }

perf/src/main/java/org/bf2/performance/InstanceProfiler.java
@@ -79,33 +79,31 @@ public class InstanceProfiler {
     public enum Profile {
         // recommended latency optimized profile
         LATENCY("acks=1\n", "auto.offset.reset=earliest\nenable.auto.commit=false\n",
-                "min.insync.replicas=2\ncompression.type=uncompressed\ncleanup.policy=delete\nretention.ms=240000\n", 25),
+                "min.insync.replicas=2\ncompression.type=uncompressed\ncleanup.policy=delete\nretention.ms=240000\n"),
         // recommended latency optimized profile, but with batching disabled for throughput testing
         LATENCY_NO_BATCHING("acks=1\nbatch.size=0\n", "auto.offset.reset=earliest\nenable.auto.commit=false\n",
-                "min.insync.replicas=2\ncompression.type=uncompressed\ncleanup.policy=delete\nretention.ms=240000\n", 25),
+                "min.insync.replicas=2\ncompression.type=uncompressed\ncleanup.policy=delete\nretention.ms=240000\n"),
         // using both https://www.confluent.io/blog/configure-kafka-to-minimize-latency/
         // and https://docs.confluent.io/cloud/current/client-apps/optimizing/throughput.html#optimizing-for-throughput
         THROUGHPUT("acks=1\nbatch.size=200000\nlinger.ms=100\n",
                 "fetch.min.bytes=100000\nauto.offset.reset=earliest\nenable.auto.commit=false\n",
-                "min.insync.replicas=2\ncompression.type=uncompressed\ncleanup.policy=delete\nretention.ms=240000\n", 200);
+                "min.insync.replicas=2\ncompression.type=uncompressed\ncleanup.policy=delete\nretention.ms=240000\n");
 
         final String producerConfig;
         final String consumerConfig;
         final String topicConfig;
-        final int catchUpToleranceMs;
         // must be changed together - and we may adapt the logic to use a spectrum of values
         final int messageSize;
         final String payloadFile;
 
-        Profile(String producerConfig, String consumerConfig, String topicConfig, int catchUpToleranceMs) {
-            this(producerConfig, consumerConfig, topicConfig, catchUpToleranceMs, 1024, "src/test/resources/payload/payload-1Kb.data");
+        Profile(String producerConfig, String consumerConfig, String topicConfig) {
+            this(producerConfig, consumerConfig, topicConfig, 1024, "src/test/resources/payload/payload-1Kb.data");
         }
 
-        Profile(String producerConfig, String consumerConfig, String topicConfig, int catchUpToleranceMs, int messageSize, String payloadFile) {
+        Profile(String producerConfig, String consumerConfig, String topicConfig, int messageSize, String payloadFile) {
             this.producerConfig = producerConfig;
             this.consumerConfig = consumerConfig;
             this.topicConfig = topicConfig;
-            this.catchUpToleranceMs = catchUpToleranceMs;
             this.messageSize = messageSize;
             this.payloadFile = payloadFile;
         }
@@ -480,7 +478,8 @@ public class InstanceProfiler {
     protected void sizeInstance() throws Exception {
         Stream<Node> workerNodes = kafkaCluster.getWorkerNodes().stream();
         if (!collocateBrokerWithZookeeper){
-            workerNodes = workerNodes.filter(n -> n.getSpec().getTaints().stream()
+            kafkaProvisioner.validateClusterForBrokers(numberOfBrokers, false, workerNodes);
+            workerNodes = kafkaCluster.getWorkerNodes().stream().filter(n -> n.getSpec().getTaints().stream()
                         .anyMatch(t -> t.getKey().equals(ManagedKafkaProvisioner.KAFKA_BROKER_TAINT_KEY)));
         }
 

perf/src/main/java/org/bf2/performance/ManagedKafkaProvisioner.java
@@ -50,11 +50,14 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Provisioner for {@link ManagedKafka} instances
  */
 public class ManagedKafkaProvisioner {
+
+    public static final int ZONES = 3;
 
     private static final String KAS_FLEETSHARD_CONFIG = "kas-fleetshard-config";
     public static final String KAFKA_BROKER_TAINT_KEY = "org.bf2.operator/kafka-broker";
@@ -333,39 +336,7 @@ public class ManagedKafkaProvisioner {
         List<Node> workers = cluster.getWorkerNodes();
 
         // divide the nodes by their zones, then sort them by their cpu availability then mark however brokers needed for the taint
-        Map<String, List<Node>> zoneAwareNodeList = workers.stream()
-                .collect(Collectors.groupingBy(n -> n.getMetadata().getLabels().get("topology.kubernetes.io/zone")));
-        zoneAwareNodeList.values()
-                .forEach(list -> Collections.sort(list,
-                        (n1, n2) -> Long.compare(TestUtils.getMaxAvailableResources(n1).cpuMillis,
-                                TestUtils.getMaxAvailableResources(n2).cpuMillis)));
-
-        for (List<Node> nodes : zoneAwareNodeList.values()) {
-            int brokersPerZone = profile.getKafka().getReplicas() / 3;
-            if (nodes.size() < brokersPerZone) {
-                throw new IllegalStateException("Not enough nodes per zone available");
-            }
-
-            for (int i = 1; i <= brokersPerZone; i++) {
-                Node n = nodes.get(nodes.size() - i);
-
-                if (!profile.getKafka().isColocateWithZookeeper()) {
-                    cluster.kubeClient()
-                            .client()
-                            .nodes()
-                            .withName(n.getMetadata().getName())
-                            .patch(PatchContext.of(PatchType.STRATEGIC_MERGE),
-                                    new NodeBuilder()
-                                            .editOrNewSpec()
-                                            .addNewTaint()
-                                            .withKey(KAFKA_BROKER_TAINT_KEY)
-                                            .withEffect("NoExecute")
-                                            .endTaint()
-                                            .endSpec()
-                                            .build());
-                }
-            }
-        }
+        validateClusterForBrokers(profile.getKafka().getReplicas(), profile.getKafka().isColocateWithZookeeper(), workers.stream());
 
         // convert the profile into simple configmap values
         ConfigMap override = toConfigMap(profile);
@@ -386,6 +357,42 @@ public class ManagedKafkaProvisioner {
         fleetshardOperatorDeployment.scale(1, true);
 
         //fleetshardOperatorDeployment.waitUntilReady(30, TimeUnit.SECONDS);
+    }
+
+    public void validateClusterForBrokers(int brokers, boolean colocateWithZookeeper, Stream<Node> workers) {
+        Map<String, List<Node>> zoneAwareNodeList = workers
+                .collect(Collectors.groupingBy(n -> n.getMetadata().getLabels().get("topology.kubernetes.io/zone")));
+        zoneAwareNodeList.values()
+                .forEach(list -> Collections.sort(list,
+                        (n1, n2) -> Long.compare(TestUtils.getMaxAvailableResources(n1).cpuMillis,
+                                TestUtils.getMaxAvailableResources(n2).cpuMillis)));
+
+        int brokersPerZone = brokers / ZONES;
+        for (List<Node> nodes : zoneAwareNodeList.values()) {
+            if (nodes.size() < brokersPerZone) {
+                throw new IllegalStateException("Not enough nodes per zone available");
+            }
+
+            for (int i = 1; i <= brokersPerZone; i++) {
+                Node n = nodes.get(nodes.size() - i);
+
+                if (!colocateWithZookeeper) {
+                    cluster.kubeClient()
+                            .client()
+                            .nodes()
+                            .withName(n.getMetadata().getName())
+                            .patch(PatchContext.of(PatchType.STRATEGIC_MERGE),
+                                    new NodeBuilder()
+                                            .editOrNewSpec()
+                                            .addNewTaint()
+                                            .withKey(KAFKA_BROKER_TAINT_KEY)
+                                            .withEffect("NoExecute")
+                                            .endTaint()
+                                            .endSpec()
+                                            .build());
+                }
+            }
+        }
     }
 
     ManagedKafkaDeployment deployCluster(String namespace, ManagedKafka managedKafka) throws Exception {

commit b2ace5c54b1ea3efb7dc427184093c1da16af45e

fixing a regression from #550 the node placement is still needed

operator/src/main/java/org/bf2/operator/managers/IngressControllerManager.java
@@ -339,6 +339,18 @@ public class IngressControllerManager {
                 .endEndpointPublishingStrategy()
             .endSpec();
 
+        if (topologyValue != null && !topologyValue.isEmpty()) {
+            builder
+                .editSpec()
+                    .withNewNodePlacement()
+                        .editOrNewNodeSelector()
+                            .addToMatchLabels(TOPOLOGY_KEY, topologyValue)
+                            .addToMatchLabels(WORKER_NODE_LABEL, "")
+                        .endNodeSelector()
+                    .endNodePlacement()
+                .endSpec();
+        }
+
         return builder.build();
     }
 

operator/src/test/java/org/bf2/operator/managers/IngressControllerManagerTest.java
@@ -16,6 +16,7 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTestResource(KubernetesServerTestResource.class)
 @QuarkusTest
@@ -54,5 +55,13 @@ public class IngressControllerManagerTest {
 
         List<IngressController> ingressControllers = openShiftClient.operator().ingressControllers().inNamespace(IngressControllerManager.INGRESS_OPERATOR_NAMESPACE).list().getItems();
         assertEquals(4, ingressControllers.size(), "Expected 4 IngressControllers: one per zone, and one multi-zone");
+
+        // make sure the zone specific has node placements
+        assertTrue(ingressControllers.stream().allMatch(c -> {
+            if (c.getMetadata().getName().equals("kas")) {
+                return true;
+            }
+            return c.getSpec().getNodePlacement() != null;
+        }));
     }
 }