7d22b45e9704c20e5edc25295a2e500c8d1efd29
kwall <kwall@apache.org>
2021-12-22 09:10:49 +0000

    adding unit test

--- operator/src/test/java/org/bf2/operator/operands/KafkaInstanceTest.java
+++ operator/src/test/java/org/bf2/operator/operands/KafkaInstanceTest.java
@@ -1,21 +1,40 @@
 package org.bf2.operator.operands;
 
+import io.javaoperatorsdk.operator.api.Context;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 import org.bf2.operator.resources.v1alpha1.ManagedKafka;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaCondition.Reason;
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaCondition.Status;
 import org.bf2.operator.utils.ManagedKafkaUtils;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 
-import java.io.IOException;
+import javax.inject.Inject;
+
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.inOrder;
 
+@QuarkusTest
 public class KafkaInstanceTest {
 
+    @InjectMock
+    KafkaCluster kafkaCluster;
+
+    @InjectMock
+    Canary canary;
+
+    @InjectMock
+    AdminServer adminServer;
+
+    @Inject
+    KafkaInstance kafkaInstance;
+
     @Test
-    void testStatus() throws IOException {
+    void testStatus() {
         // sample operand readiness
         Operand<ManagedKafka> error = mockOperand(new OperandReadiness(Status.False, Reason.Error, "I'm not well"));
         Operand<ManagedKafka> installing = mockOperand(new OperandReadiness(Status.False, Reason.Installing, "I'm installing"));
@@ -45,6 +64,18 @@ public class KafkaInstanceTest {
         assertEquals(Status.Unknown, readiness.getStatus());
         assertEquals(Reason.StrimziUpdating, readiness.getReason());
         assertEquals("I don't know", readiness.getMessage());
+    }
+
+    @Test
+    void deleteOrderCanaryDeleteBeforeKafkaCluster() {
+        Context<ManagedKafka> context = Mockito.mock(Context.class);
+        ManagedKafka managedKafka = ManagedKafkaUtils.dummyManagedKafka("x");
+
+        kafkaInstance.delete(managedKafka, context);
+
+        InOrder inOrder = inOrder(canary, kafkaCluster);
+        inOrder.verify(canary).delete(managedKafka, context);
+        inOrder.verify(kafkaCluster).delete(managedKafka, context);
     }
 
     private OperandReadiness getReadiness(Operand<ManagedKafka>... operands) {
