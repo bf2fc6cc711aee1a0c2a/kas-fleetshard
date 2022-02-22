8137e4e434d4b1c733475a8831fc7a628d26a0a4
Steve Hawkins <shawkins@redhat.com>
2022-02-22 08:42:48 -0500

    adding a test of backwards compatibility

--- api/src/test/java/org/bf2/resources/ManagedKafkaSpecTest.java
+++ api/src/test/java/org/bf2/resources/ManagedKafkaSpecTest.java
@@ -1,6 +1,7 @@
 package org.bf2.resources;
 
 import org.bf2.operator.resources.v1alpha1.ManagedKafkaSpec;
+import org.bf2.operator.resources.v1alpha1.ManagedKafkaSpecBuilder;
 import org.bf2.operator.resources.v1alpha1.Versions;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ManagedKafkaSpecTest {
 
-    @Test public void testEquals() {
+    @Test
+    public void testEquals() {
         ManagedKafkaSpec spec1 = new ManagedKafkaSpec();
         ManagedKafkaSpec spec2 = new ManagedKafkaSpec();
         // if it's not equals, then the default equality is being used
@@ -21,6 +23,17 @@ public class ManagedKafkaSpecTest {
 
         spec1.getVersions().setKafka("2.2.2");
         assertNotEquals(spec1, spec2);
+    }
+
+    @Test
+    public void testIngressEgressBackwardsCompatibility() {
+        ManagedKafkaSpec spec = new ManagedKafkaSpecBuilder().withNewCapacity()
+                .withNewIngressEgressThroughputPerSec("30Mi")
+                .endCapacity()
+                .build();
+
+        assertEquals(spec.getCapacity().getIngressEgressThroughputPerSec(), spec.getCapacity().getIngressPerSec());
+        assertEquals(spec.getCapacity().getIngressEgressThroughputPerSec(), spec.getCapacity().getEgressPerSec());
     }
 
 }
