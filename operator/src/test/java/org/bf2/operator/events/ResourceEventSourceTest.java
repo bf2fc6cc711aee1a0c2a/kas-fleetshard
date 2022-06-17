2e2a755a63afc0403c8027aaf844f24f429dfbcc
Steve Hawkins <shawkins@redhat.com>
2022-06-17 15:17:43 -0400

    MGDSTRM-8876 changing all child events to updates

--- operator/src/test/java/org/bf2/operator/events/ResourceEventSourceTest.java
+++ operator/src/test/java/org/bf2/operator/events/ResourceEventSourceTest.java
@@ -5,6 +5,8 @@ import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.javaoperatorsdk.operator.processing.event.Event;
 import io.javaoperatorsdk.operator.processing.event.EventHandler;
 import io.javaoperatorsdk.operator.processing.event.ResourceID;
+import io.javaoperatorsdk.operator.processing.event.source.controller.ResourceAction;
+import io.javaoperatorsdk.operator.processing.event.source.controller.ResourceEvent;
 import io.quarkus.test.junit.QuarkusTest;
 import org.bf2.common.OperandUtils;
 import org.junit.jupiter.api.Test;
@@ -39,6 +41,26 @@ class ResourceEventSourceTest {
         Mockito.verify(mockEventHandler).handleEvent(eventCaptor.capture());
 
         assertEquals(new ResourceID("parent", "ns"), eventCaptor.getValue().getRelatedCustomResourceID());
+    }
+
+    @Test void testDelete() {
+        EventHandler mockEventHandler = Mockito.mock(EventHandler.class);
+        resourceEventSource.setEventHandler(mockEventHandler);
+
+        ConfigMap parent = new ConfigMapBuilder().withNewMetadata().withName("parent").withUid("uid").endMetadata().build();
+        ConfigMap old = new ConfigMapBuilder().withNewMetadata().withName("old").withNamespace("ns").endMetadata().build();
+        OperandUtils.setAsOwner(parent, old);
+
+        resourceEventSource.onDelete(old, false);
+
+        // make sure we know that the parent was modified
+
+        ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
+        Mockito.verify(mockEventHandler).handleEvent(eventCaptor.capture());
+
+        ResourceEvent event = (ResourceEvent) eventCaptor.getValue();
+        assertEquals(new ResourceID("parent", "ns"), event.getRelatedCustomResourceID());
+        assertEquals(ResourceAction.UPDATED, event.getAction()); // parent is updated, not deleted
     }
 
 }
