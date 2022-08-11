6918547f5c2c5fe7c83ad9a1ce1e493b2e4f9f3a
kwall <kwall@apache.org>
2022-08-11 18:35:14 +0100

    remove now redundant broker plugins properties

--- operator/src/main/java/org/bf2/operator/operands/KafkaInstanceConfiguration.java
+++ operator/src/main/java/org/bf2/operator/operands/KafkaInstanceConfiguration.java
@@ -432,9 +432,6 @@ public class KafkaInstanceConfiguration {
         protected String authorizerClass = null;
         @JsonProperty("broker-plugins-config-prefix")
         protected String brokerPluginsConfigPrefix = null;
-        @Deprecated
-        @JsonProperty("config-prefix")
-        protected String configPrefix = null;
         @JsonProperty("global")
         protected String global = null;
         @JsonProperty("owner")
@@ -508,16 +505,6 @@ public class KafkaInstanceConfiguration {
 
         public void setBrokerPluginsConfigPrefix(String brokerPluginsConfigPrefix) {
             this.brokerPluginsConfigPrefix = brokerPluginsConfigPrefix;
-        }
-
-        @Deprecated
-        public String getConfigPrefix() {
-            return configPrefix;
-        }
-
-        @Deprecated
-        public void setConfigPrefix(String configPrefix) {
-            this.configPrefix = configPrefix;
         }
 
         public String getGlobal() {
